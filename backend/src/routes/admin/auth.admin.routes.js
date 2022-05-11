a06cd39b7ebcdd0fb99fd6945a11f815dccff53d
akash.dutta <akash.dutta@glosity.club>
2022-05-11 09:08:32 +0530

    api validator for admin api

--- backend/src/routes/admin/auth.admin.routes.js
+++ backend/src/routes/admin/auth.admin.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { signup, signin, isSignedIn } = require('../../controller/admin/auth.admin.controller');
-// const { validateSignupRequest, isRequestValidated, validateSigninRequest } = require('../validators/auth');
+const { validateSignupRequest, isRequestValidated, validateSigninRequest } = require('../../validator/auth.validator');
 const router = express.Router();
 
-router.post('/admin/signup', signup);
-router.post('/admin/signin', signin);
+router.post('/admin/signup', validateSignupRequest, isRequestValidated, signup);
+router.post('/admin/signin', validateSigninRequest, isRequestValidated, signin);
 
 router.post('/admin/profile', isSignedIn, (req, res) => {
 	res.status(200).json({ user: 'profile' });
