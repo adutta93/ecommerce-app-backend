71df692e9cd50984b33d7bbb38a72e3b089c51f9
akash.dutta <akash.dutta@glosity.club>
2022-05-13 21:42:25 +0530

    addtocart updated

--- backend/src/controller/cart.controller.js
+++ backend/src/controller/cart.controller.js
@@ -13,39 +13,32 @@ exports.addItemToCart = (req, res) => {
 	Cart.findOne({ user: req.user._id }).exec((err, cart) => {
 		if (err) return res.status(400).json({ err });
 		if (cart) {
-			// return res.status(201).json({ message: cart });
+			let condition, update;
 			const ItemInCart = cart.cartItems.find((c) => c.product == req.body.cartItems.product);
 			if (ItemInCart) {
-				Cart.findOneAndUpdate(
-					{ user: req.user._id, 'cartItems.product': req.body.cartItems.product },
-					{
-						$set: {
-							'cartItems.$': {
-								...req.body.cartItems,
-								quantity: ItemInCart.quantity + req.body.cartItems.quantity,
-							},
+				condition = { user: req.user._id, 'cartItems.product': req.body.cartItems.product };
+				update = {
+					$set: {
+						'cartItems.$': {
+							...req.body.cartItems,
+							quantity: ItemInCart.quantity + req.body.cartItems.quantity,
 						},
-					}
-				).exec((err, _cart) => {
-					if (err) return res.status(400).json({ err });
-					if (_cart) return res.status(201).json({ cart: _cart });
-				});
+					},
+				};
 			} else {
-				Cart.findOneAndUpdate(
-					{ user: req.user._id },
-					{
-						$push: {
-							cartItems: req.body.cartItems,
-						},
-					}
-				).exec((err, _cart) => {
-					if (err) return res.status(400).json({ err });
-					if (_cart) {
-						return res.status(200).json({ cart: _cart });
-					}
-				});
-				// return res.status(201).json({ cart: cart });
+				condition = { user: req.user._id };
+				update = {
+					$push: {
+						cartItems: req.body.cartItems,
+					},
+				};
 			}
+			Cart.findOneAndUpdate(condition, update).exec((err, _cart) => {
+				if (err) return res.status(400).json({ err });
+				if (_cart) {
+					return res.status(200).json({ cart: _cart });
+				}
+			});
 		} else {
 			//if cart not exists
 			const cart = new Cart({
@@ -60,4 +53,28 @@ exports.addItemToCart = (req, res) => {
 			});
 		}
 	});
+};
+
+exports.getCartItems = (req, res) => {
+	//const { user } = req.body.payload;
+	//if(user){
+	Cart.findOne({ user: req.user._id })
+		.populate('cartItems.product', '_id name price productPictures')
+		.exec((error, cart) => {
+			if (error) return res.status(400).json({ error });
+			if (cart) {
+				let cartItems = {};
+				cart.cartItems.forEach((item, index) => {
+					cartItems[item.product._id.toString()] = {
+						_id: item.product._id.toString(),
+						name: item.product.name,
+						img: item.product.productPictures[0].img,
+						price: item.product.price,
+						qty: item.quantity,
+					};
+				});
+				res.status(200).json({ cartItems });
+			}
+		});
+	//}
 };
--- backend/src/routes/cart.routes.js
+++ backend/src/routes/cart.routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { addItemToCart } = require('../controller/cart.controller');
+const { addItemToCart, getCartItems } = require('../controller/cart.controller');
 const { isSignedIn } = require('../middleware');
 const router = express.Router();
 
 router.post('/cart/add-to-cart', isSignedIn, addItemToCart);
 //router.post('/user/cart/addToCartByLogin', requireSignin, userMiddleware, addToCart);
-
+router.post('/cart/get-Cart-Items', isSignedIn, getCartItems);
 module.exports = router;
